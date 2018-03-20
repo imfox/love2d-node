a5d6f3148ccb5b33b2571d94cb90bb4e6f630e3a
fox- <1127001585@qq.com>
2018-03-20 22:11:12 +0800

    漏传

---
+++ src/ui/Label.lua
@@ -0,0 +1,51 @@
+---
+--- Created by fox.
+--- DateTime: 2018/3/20 20:53
+---
+---
+--- Created by fox.
+--- DateTime: 2018/3/16 21:22
+---
+local import = require("Import");
+
+---@type Loader
+local Loader = import("..core.Loader");
+
+local class = import("..class");
+
+---@type AutoBitmap
+local AutoBitmap = import(".AutoBitmap")
+
+---@type Drawable
+local Drawable = import("..core.Drawable")
+
+---@class Label : Drawable
+local Label = class(Drawable)
+
+---@param this Label
+function Label.ctor(this)
+    this.graphics = AutoBitmap.new();
+    this.text = "";
+end
+
+---@field public graphics AutoBitmap
+
+---@param this Label
+---@return Label
+function Label._render(this)
+    if not this.visible or this.destroyed or this.alpha == 0 or this.scaleY ==0 or this.scaleX == 0 then    -- 已经不会显示出来了
+        return this;
+    end
+    this.graphics.one = {cmd="print",args={this.text,0,0,0,1,1,this.pivotX,this.pivotY}};
+    this.graphics:_begin(this.x,this.y,this.rotation,this.scaleX,this.scaleY):_render();
+    for _,drawable in ipairs(this.components) do
+        if drawable._render then
+            drawable:_render()
+        end
+    end
+    this.graphics:_end();
+    return this;
+end
+
+
+return Label;
