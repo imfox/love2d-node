88c1196a62def69030bae5287bbfb25efc9d9b6b
fox- <1127001585@qq.com>
2018-03-28 23:18:05 +0800

    添加一个缩放按钮类,可能还不够完善

---
+++ src/nodeFrame/ui/ScaleButton.lua
@@ -0,0 +1,35 @@
+---
+--- Created by fox.
+--- DateTime: 2018/3/27 21:43
+---
+
+local class = require("nodeFrame.class");
+
+---@type Sprite
+local Image = require("nodeFrame.ui.Image")
+
+---@class ScaleButton : Sprite
+local ScaleButton = class(Image)
+
+local Tween = require("nodeFrame.core.Utils.Tween");
+local Ease = require("nodeFrame.core.Utils.Ease");
+
+---@param this ScaleButton
+---@param skin string
+function ScaleButton.ctor(this,skin)
+    this:super(skin)
+
+    this.anchorX = 0.5;
+    this.anchorY = 0.5;
+
+    this:on("MOUSE_DOWN", function ()
+        Tween.to(this,{scaleX=0.8,scaleY=0.8},100,Ease.linear)
+    end)
+    this:on("MOUSE_UP", function ()
+        Tween.to(this,{scaleX=1,scaleY=1},100,Ease.inBack)
+        this._press = false;
+    end)
+end
+
+
+return ScaleButton;
